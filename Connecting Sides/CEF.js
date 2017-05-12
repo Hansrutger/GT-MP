9606f96d66bf955d72f42ce1f08359b9d6fca439
Hansrutger <bloody_pirate_4-ever@hotmail.com>
2017-05-12 03:46:58 +0200

    Add files via upload

---
+++ Connecting Sides/CEF.js
@@ -0,0 +1,26 @@
+﻿API.onServerEventTrigger.connect(function (eventName, args) {
+    switch (eventName) {
+        case 'testjs':
+            TestHandler();
+            break;
+    }
+
+});
+
+var browser = null;
+function TestHandler() {
+    var res = API.getScreenResolution();
+    browser = API.createCefBrowser(500, 499);
+    API.waitUntilCefBrowserInit(browser);
+    API.setCefBrowserPosition(browser, (res.Width / 2) - (500 / 2), (res.Height / 2) - (499 / 2));
+    API.loadPageCefBrowser(browser, "Designs/blackbox.html");
+    API.showCursor(true);
+    API.setCanOpenChat(false);
+}
+
+function BtnClicked() {
+    API.showCursor(false);
+    API.setCanOpenChat(true);
+    API.destroyCefBrowser(browser);
+    API.triggerServerEvent("BackToTheFuture");
+}
---
+++ Connecting Sides/CEFCommands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrandTheftMultiplayer.Server;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Math;
+
+namespace CEFResource
+{
+    public class CEFCommands : Script
+    {
+        public CEFCommands()
+        {
+            API.onClientEventTrigger += OnClientTriggered;
+        }
+
+        [Command("test")]
+        public void TestFunction(Client player)
+        {
+            API.triggerClientEvent(player, "testjs");
+        }
+
+        public void OnClientTriggered(Client player, string eventName, params object[] arguments)
+        {
+            switch (eventName)
+            {
+                case "BackToTheFuture":
+                    API.sendNotificationToPlayer(player, "We're back where we started");
+                    break;
+            }
+        }
+    }
+}
